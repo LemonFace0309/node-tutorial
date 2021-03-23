2e1d1917a4c3bc73c17af58430c96d14446a20fa
Charles Liu <charles.liu0309@gmail.com>
2021-03-23 05:21:01 -0400

    added templating engines and started experimenting with pug

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const path = require('path')
 const express = require('express') // exports a function
 // const bodyParser = require('body-parser')
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+app.set('view engine', 'pug')
+// app.set('views', 'views')
+
 app.use(express.urlencoded({extended: false})) // parses req.body
 app.use(express.static(path.join(__dirname, 'public'))) // grants read access to path
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,18 @@ const path = require('path')
 
 const express = require('express')
 
+const adminData = require('./admin')
+
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  console.log('home')
+  console.log('shop.js', adminData.products)
+  const products = adminData.products
+  res.render('shop', {prods: products, docTitle: 'Shop'})
   //res.send("<h1>Hello from Express</h1>")
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html')) 
-  // __dirname holds absolute path on os to project folder
-  // path works on linux and windows systems
+  //res.sendFile(path.join(__dirname, '..', 'views', 'shop.html')) 
+    // __dirname holds absolute path on os to project folder
+    // path works on linux and windows systems
   
 })
 
