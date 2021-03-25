ca1f020b2406fa082bcc7509d2a4aa32c39f2176
Charles Liu <charles.liu0309@gmail.com>
2021-03-25 04:50:33 -0400

    experimented a bit with handlebars. I dont like it

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const path = require('path')
 // const routes = require('./routes')
 
 const express = require('express') // exports a function
-// const bodyParser = require('body-parser')
+// const expressHbs = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main'})) // registers a new templating engine. 'pug' is built in so it's not necessary. expresshbs initializes the engine
+// app.set('view engine', 'handlebars')
 app.set('view engine', 'pug')
 // app.set('views', 'views')
 
-app.use(express.urlencoded({extended: false})) // parses req.body
+app.use(express.urlencoded({ extended: false })) // parses req.body
 app.use(express.static(path.join(__dirname, 'public'))) // grants read access to path
 
 app.use('/admin', adminData.routes)
@@ -22,7 +24,7 @@ app.use(shopRoutes)
 app.use((req, res, next) => {
   // res.status(404).send('<h1>Page not Found</h1>')
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
 // const server = http.createServer(app)
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   console.log('shop.js', adminData.products)
   const products = adminData.products
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0})
   //res.send("<h1>Hello from Express</h1>")
   //res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
   // __dirname holds absolute path on os to project folder
