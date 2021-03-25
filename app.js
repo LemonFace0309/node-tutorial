4602bf39f3575e82a0fbd28fadbbc9f58df2f17c
Charles Liu <charles.liu0309@gmail.com>
2021-03-25 06:03:21 -0400

    experimented with ejs a little. I still prefer pug but ejs isnt the worst

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express()
 
 // app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main'})) // registers a new templating engine. 'pug' is built in so it's not necessary. expresshbs initializes the engine
 // app.set('view engine', 'handlebars')
-app.set('view engine', 'pug')
+// app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 // app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: false })) // parses req.body
@@ -24,7 +25,7 @@ app.use(shopRoutes)
 app.use((req, res, next) => {
   // res.status(404).send('<h1>Page not Found</h1>')
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', { pageTitle: 'Page Not Found' })
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' })
 })
 
 // const server = http.createServer(app)
